98004eb0a6f90f73ffb03cac826e724f3ea8453c
Rob Kostermans <rob.kostermans@gmail.com>
2017-03-26 21:12:03 +0200

    details screen

---
+++ src/components/model/data.js
@@ -0,0 +1,30 @@
+var data = {
+    "paulus":{
+        title : "Paulus",
+        description:"In botany, a tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves in most species. In some usages, the definition of a tree may be narrower, including only woody plants with secondary growth, plants that are usable as lumber or plants above a specified height."
+    },
+    "fillipus":{
+        title : "Fillipus",
+        description:"A desk or bureau is a piece of furniture with a flat table-style work surface used in a school, office, home or the like for academic, professional or domestic activities such as reading, writing, or using equipment"
+    },
+    "Jakobus":{
+        title : "Jakobus",
+        description:"Middle sized meetingroom with a projecting screen and whiteboard.",
+    },
+    "petrus":{
+        title : "Petrus",
+        description:"Where people eat and socialize.",
+    },
+    "mattheus":{
+        title : "Mattheus",
+        description:"Wher people meet and drink beer.",
+    },
+    "johannes":{
+        title : "Johannes",
+        description:"Wher people meet and drink beer.",
+    },
+    "andreas":{
+        title : "Andreas",
+        description:"Wher people meet and drink beer.",
+    },
+}
--- src/components/ui/script.js
+++ src/components/ui/script.js
@@ -1,6 +1,8 @@
 
 var initNavigation = function(duration,callback){
-    var animation = new TimelineMax()
+    callback = callback || Void;
+
+    var animation = new TimelineMax();
     animation.to(
         ".left-wrap .loader", 
         duration/2,
@@ -14,9 +16,51 @@ var initNavigation = function(duration,callback){
 
 
 var ActivateNavigation = function(callback){
-    console.log(callback)
+    
+    TweenMax.to(".circle-loader-message .loading-message",0.5,{css:{opacity:0}});
+    
+    
+    setState("reset")
+    var avaiableRoomCounterContainer = document.querySelector("#availableRoomCounter");
+    avaiableRoomCounterContainer.innerHTML = avaiableRoomCounter + " meetingrooms available";
+    TweenMax.to("#availableRoomCounter",0.5,{css:{opacity:1}});
+
     callback.call(this);;
 }
+
+var setState = function(state){
+    var main = document.querySelector("main");
+    main.dataset.state = state;
+    switch (state){
+        case "explode":
+            buildingExplode();
+            break;
+        case "opaque":
+            buildingOpenAnim();
+            break;
+        default:
+            buildingReset();
+    }
+
+    //var nav = document.querySelector("nav#container");
+    //nav.dataset.state = state;
+}
+
+var showDetails = function(id){
+	if(data[id]){
+		var title = document.getElementById("title");
+		title.innerHTML = data[id].title
+		var details = document.getElementById("availableRoomCounter");
+		details.innerHTML = data[id].description
+	}
+}
+
+
+var Void = function(){
+    console.log("void fallback")
+}
+
+
 
 
 
