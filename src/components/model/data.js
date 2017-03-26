8cfc40f40af8afcca391429cfcee21acbfa22f91
Rob Kostermans <rob.kostermans@gmail.com>
2017-03-26 23:47:41 +0200

    data details

--- src/components/model/data.js
+++ src/components/model/data.js
@@ -1,30 +1,30 @@
 var data = {
     "paulus":{
         title : "Paulus",
-        description:"In botany, a tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves in most species. In some usages, the definition of a tree may be narrower, including only woody plants with secondary growth, plants that are usable as lumber or plants above a specified height."
+        description:"Large meemtingroom for 12 people, whiteboard and screen."
     },
     "fillipus":{
         title : "Fillipus",
-        description:"A desk or bureau is a piece of furniture with a flat table-style work surface used in a school, office, home or the like for academic, professional or domestic activities such as reading, writing, or using equipment"
+        description:"Project workroom for 8 people with whiteboard and screen. can be closed off with curtain."
     },
     "Jakobus":{
         title : "Jakobus",
-        description:"Middle sized meetingroom with a projecting screen and whiteboard.",
+        description:"Project workroom for 8 people with whiteboard screen. can be closed off with curtain.",
     },
     "petrus":{
         title : "Petrus",
-        description:"Where people eat and socialize.",
+        description:"Demo room for 8 people with screen and small whiteboard.",
     },
     "mattheus":{
         title : "Mattheus",
-        description:"Wher people meet and drink beer.",
+        description:"Small meetingroom for 4 people with screen.",
     },
     "johannes":{
-        title : "Johannes",
+        title : "Small meeting room for 4 people with whiteboard but no screen.",
         description:"Wher people meet and drink beer.",
     },
     "andreas":{
         title : "Andreas",
-        description:"Wher people meet and drink beer.",
+        description:"Lounge room (no table) with screen and whiteboard.",
     },
 }
